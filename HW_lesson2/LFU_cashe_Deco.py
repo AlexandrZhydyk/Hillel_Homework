7b212e63a7f63fbc266880d4aeffcf41e084d75e
Alexandr Zhydyk <zhydykalex@ukr.net>
2022-06-28 20:15:06 +0300

    Lesson2

---
+++ HW_lesson2/LFU_cashe_Deco.py
@@ -0,0 +1,43 @@
+import functools
+import requests
+
+def cache(max_limit=3):
+    def internal(f):
+        @functools.wraps(f)
+        def deco(*args, **kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+            # Увеличение counter при повторном запросе
+            if cache_key in deco._cache:
+                deco._cache[cache_key]["counter"] += 1
+                return deco._cache[cache_key]["rez"]
+            # Удаление элемента при достижении лимита
+            if len(deco._cache) >= max_limit:
+                del deco._cache[min(deco._cache, key=lambda x: deco._cache[x]["counter"])]
+            # Добавление нового элемента
+            deco._cache[cache_key] = {}
+            result = f(*args, **kwargs)
+            deco._cache[cache_key]["counter"] = 1
+            deco._cache[cache_key]["rez"] = result
+            print(deco._cache)
+            return result
+        deco._cache = {}
+        return deco
+    return internal
+
+@cache()
+def fetch_url(url, first_n=100):
+    """Fetch a given url"""
+    res = requests.get(url)
+    return res.content[:first_n] if first_n else res.content
+
+
+print(fetch_url("https://www.google.com"))
+print(fetch_url("https://www.google.com/maps"))
+print(fetch_url("https://www.youtube.com"))
+print(fetch_url("https://www.google.com/maps"))
+print(fetch_url("https://www.google.com/maps"))
+print(fetch_url("https://www.youtube.com"))
+print(fetch_url("https://github.com"))
+
+
+
---
+++ HW_lesson2/MemoDeco.py
@@ -0,0 +1,26 @@
+import os
+import psutil
+
+def mem_measur(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Создаем экземпляр класса, передавая ему id текущего процесса
+        process = psutil.Process(os.getpid())
+        # Размер выделенной памяти до выполнения ф-ции
+        init_memo = process.memory_info()[0]
+        result = func(*args, **kwargs)
+        # Размер выделенной памяти после выполнения ф-ции
+        end_memo = process.memory_info()[0]
+        # Занятый размер памяти процессом
+        occup_memo = (end_memo - init_memo)
+        print(f'Function {func.__name__} takes {occup_memo} bytes')
+        return result
+    return wrapper
+
+@mem_measur
+def fetch_url(url, first_n=100):
+    """Fetch a given url"""
+    res = requests.get(url)
+    return res.content[:first_n] if first_n else res.content
+
+print(fetch_url("https://www.google.com/maps"))
