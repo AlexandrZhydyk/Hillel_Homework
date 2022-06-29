35335757909620426c2b49b42af5dc6117f95a7f
Bogdan <bshevchenko12@gmail.com>
2022-06-29 10:36:59 +0300

    Corrections

--- HW_lesson2/MemoDeco.py
+++ HW_lesson2/MemoDeco.py
@@ -1,21 +1,25 @@
-import os
 import psutil
+import functools
+import requests
+
 
 def mem_measur(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         # Создаем экземпляр класса, передавая ему id текущего процесса
-        process = psutil.Process(os.getpid())
+        process = psutil.Process()  # Можна не передавати pid в аргумент конструктора. Він візьме поточній pid.
         # Размер выделенной памяти до выполнения ф-ции
-        init_memo = process.memory_info()[0]
+        init_memo = process.memory_info().rss  # Краще використовувати імена, а не індекси, якщо є така можливість
         result = func(*args, **kwargs)
         # Размер выделенной памяти после выполнения ф-ции
-        end_memo = process.memory_info()[0]
+        end_memo = process.memory_info().rss
         # Занятый размер памяти процессом
         occup_memo = (end_memo - init_memo)
         print(f'Function {func.__name__} takes {occup_memo} bytes')
         return result
+
     return wrapper
+
 
 @mem_measur
 def fetch_url(url, first_n=100):
@@ -23,4 +27,11 @@ def fetch_url(url, first_n=100):
     res = requests.get(url)
     return res.content[:first_n] if first_n else res.content
 
-print(fetch_url("https://www.google.com/maps"))
+
+if __name__ == '__main__':
+    # Без цієї конструкції імпортований модуль виконується одразу при імпорті
+    # У більшості випадків треба використовувати цю конструкцію та викликати
+    # об'явлені функції у ній.
+    #
+    # У PyCharm є шорткат для цього. Просто пишеш "main" та Enter
+    print(fetch_url("https://www.google.com/maps"))
---
+++ HW_lesson2/README.md
@@ -0,0 +1,10 @@
+### Іменування директорій та файлів (пакетов та модулів)
+
+У [цій](https://github.com/naming-convention/naming-convention-guides/tree/master/python#tldr) 
+табличці можеш знайти швидкий реферес по іменованню.
+
+Файли та папки з "-" в середені взагалі не можна імпортувати через
+```python
+from
+import 
+```
